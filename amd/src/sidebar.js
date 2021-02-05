83aa2220eda35ff8944cc4a1a2e710d6d9140287
rismantrdr <rismantrdr@gmail.com>
2021-02-05 19:39:46 +0700

    next

--- amd/src/sidebar.js
+++ amd/src/sidebar.js
@@ -1,18 +1,29 @@
 const sidebarButton = document.getElementById('sidebar-btn')
 const sidebarPanel = document.getElementById('nav-drawer')
-const body = document.getElementsByTagName('body')[0]
+const body = document.querySelector('body')
 const navigation = document.getElementById('my_navbar')
+    // const iconDrawer = document.querySelectorAll('#nav-drawer #sidebar ul li a .media-left')
+const iconDrawer = document.querySelectorAll('.media-left')
 
-// controll navigation 
 if (body.classList.contains('drawer-open-left')) {
+    // icon 
+    for (let i = 0; i < iconDrawer.length; i++) {
+        iconDrawer[i].classList.add('drawer-close-icon')
+    }
+    // navigation 
     navigation.classList.add('my-drawer-open')
-} else if (body.classList.contains('risman')) {
+} else if (body.classList.contains('')) {
+    // navigation 
     navigation.classList.remove('my-drawer-open')
 }
 sidebarButton.addEventListener('click', (e) => {
     if (body.classList.contains('drawer-open-left')) {
         navigation.classList.remove('my-drawer-open')
     } else {
+        // icon 
+        for (let i = 0; i < iconDrawer.length; i++) {
+            iconDrawer[i].classList.add('drawer-close-icon')
+        }
         navigation.classList.add('my-drawer-open')
     }
     // button animation
